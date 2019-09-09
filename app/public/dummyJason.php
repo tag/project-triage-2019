cbbdf7e6afb24ed1359eb555165b48f85612fb4a
Roxie Liu <roxiliu@iu.edu>
2019-09-09 14:16:36 -0400

    add []

--- app/public/dummyJason.php
+++ app/public/dummyJason.php
@@ -1,21 +1,23 @@
-{
-  "patientGuid": "SOME-REALLY-SHORT-1",
-  "firstName": "Jason",
-  "lastName": "Dummuy",
-  "dob": "2006-09-03",
-  "sexAtBirth": "Male"
-}，
-{
-  "patientGuid": "SOME-REALLY-SHORT-2",
-  "firstName": "Roxie",
-  "lastName": "Liu",
-  "dob": "2009-09-03",
-  "sexAtBirth": "Male"
-}，
-{
-  "patientGuid": "SOME-REALLY-SHORT-3",
-  "firstName": "Griffin",
-  "lastName": "Jung",
-  "dob": "1998-09-03",
-  "sexAtBirth": "Male"
-}
+[
+  {
+    "patientGuid": "SOME-REALLY-SHORT-1",
+    "firstName": "Jason",
+    "lastName": "Dummuy",
+    "dob": "2006-09-03",
+    "sexAtBirth": "Male"
+  }，
+  {
+    "patientGuid": "SOME-REALLY-SHORT-2",
+    "firstName": "Roxie",
+    "lastName": "Liu",
+    "dob": "2009-09-03",
+    "sexAtBirth": "Male"
+  }，
+  {
+    "patientGuid": "SOME-REALLY-SHORT-3",
+    "firstName": "Griffin",
+    "lastName": "Jung",
+    "dob": "1998-09-03",
+    "sexAtBirth": "Male"
+  }
+]
