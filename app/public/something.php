410c5f3eb0f29283efc9b857df1c10d7f5e0a963
bliow <54509960+bliow@users.noreply.github.com>
2019-09-11 10:34:28 -0400

    Create something.php

---
+++ app/public/something.php
@@ -0,0 +1,23 @@
+[
+  {
+    "patientGuid": "782f1b2b-5df3-4e89-a0ad-7a4b90742dfe",
+    "firstName": "Bjorn",
+    "lastName": "Liow",
+    "dob": "1996-02-20",
+    "sexAtBirth": "M",
+  },
+  {
+    "patientGuid": "794f1c4b-6gf3-2f56-a2de-8fas62382ghr",
+    "firstName": "Pearl",
+    "lastName": "Koc",
+    "dob": "1999-06-14",
+    "sexAtBirth": "F",
+  },
+  {
+    "patientGuid": "5321c2y4g-8yt3-4u90-b3ga-98dahcb2dfe",
+    "firstName": "Milo",
+    "lastName": "Ham",
+    "dob": "2017-04-20",
+    "sexAtBirth": "M",
+  }
+]
