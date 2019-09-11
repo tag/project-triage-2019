bb2d477863f80e8f2ec6efff18cc0c1567a58b35
Tom G <tomg@byu.net>
2019-09-11 09:45:00 -0400

    Added fetch API

--- app/public/js/waiting.app.js
+++ app/public/js/waiting.app.js
@@ -1,28 +1,22 @@
 var waitingApp = new Vue({
   el: '#patientWaitingApp',
   data: {
-    patients: [
-      {
-        "patientGuid": "SOME-REALLY-LONG-1234",
-        "firstName": "Sylvia",
-        "lastName": "Hernandez",
-        "dob": "2012-09-01",
-        "sexAtBirth": "F"
-      },
-      {
-        "patientGuid": "SOME-REALLY-SHORT-5678",
-        "firstName": "Vish",
-        "lastName": "Balasubramanian",
-        "dob": "1950-12-15",
-        "sexAtBirth": "M"
-      },
-      {
-        "patientGuid": "SOME-UNIQUE-ABCDE1",
-        "firstName": "John",
-        "lastName": "Doe",
-        "dob": "1997",
-        "sexAtBirth": "M"
-      }
-    ]
+    patients: []
+  },
+  methods: {
+    fetchPatients() {
+      fetch('dummy.php')
+      .then(response => response.json())
+      .then(json => {waitingApp.patients = json});
+
+      // Means the same at this
+      // fetch('https://randomuser.me/api/')
+      // .then(function(response) {return response.json()})
+      // .then(function(json) {waitingApp.people = json});
+
+    }
+  },
+  created() {
+    this.fetchPatients()
   }
 });
