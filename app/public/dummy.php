25b44bcff26ff5011f97594a4571eceb5b0512e7
Yanan Nie <niey@iu.edu>
2019-09-11 11:21:12 -0400

    9/11
    Include Assignment 3 Instruction

---
+++ app/public/dummy.php
@@ -0,0 +1,27 @@
+
+[
+  {
+    "patientGuid" : "SOME-REALLY-SHORT-1234",
+    "firstName" : "Betty",
+    "lastName" : "Nie",
+    "dob" : "1997-11-10",
+    "sexAtBirth" : "F",
+    "priority" : "high"
+  }
+  {
+    "patientGuid" : "SOME-REALLY-LONG-5678",
+    "firstName" : "Peter",
+    "lastName" : "Shi",
+    "dob" : "1997-03-27",
+    "sexAtBirth" : "M",
+    "priority" : "medium"
+  }
+  {
+    "patientGuid" : "SOME-UNIQUE-1996",
+    "firstName" : "Tiffany",
+    "lastName" : "Fu",
+    "dob" : "1996-01-18",
+    "sexAtBirth" : "F",
+    "priority" : "low"
+  }
+]
