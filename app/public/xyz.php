659ba3a4c9c05a44dac5810202dad46d0c79d3dc
Yashiel Kalyani <kalyaniyashiel9@gmail.com>
2019-09-11 13:21:09 -0400

    Class update
    Updated code as done in class

---
+++ app/public/xyz.php
@@ -0,0 +1,26 @@
+[
+    {
+      "patientGuid":"123",
+      "firstName":"Alex",
+      "lastName":"Zubi",
+      "dob":"1996-12-09",
+      "sexAtBirth":"M"
+    },
+
+    {
+      "patientGuid":"234",
+      "firstName":"Bob",
+      "lastName":"Gregory",
+      "dob":"1997-01-20",
+      "sexAtBirth":"F"
+    },
+
+    {
+      "patientGuid":"345",
+      "firstName":"Clide",
+      "lastName":"Bala",
+      "dob":"1998-09-01",
+      "sexAtBirth":"F"
+    }
+
+]
