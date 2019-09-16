4fe1500d7bf1dec47d38917784f6967b1c87a9bb
Jason Murner <jmurner@iu.edu>
2019-09-16 11:18:16 -0400

    Class 9/16

--- app/public/js/waitingApp.js
+++ app/public/js/waitingApp.js
@@ -5,9 +5,28 @@ var waitingApp = new Vue({
   },
   methods: {
     fetchPatients() {
-      fetch('dummy.php')
+      fetch('dummy.json')
       .then(response => response.json())
       .then(json => { waitingApp.patients = json })
+    }
+  },
+  created() {
+    this.fetchPatients();
+  }
+});
+
+
+
+var patientRecordApp = new Vue({
+  el: '#patientRecordApp',
+  data: {
+    patients: []
+  },
+  methods: {
+    fetchPatients() {
+      fetch('dummy.json')
+      .then(response => response.json())
+      .then(json => { patientRecordApp.patients = json })
     }
   },
   created() {
