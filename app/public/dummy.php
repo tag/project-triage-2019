34ccd22dfe89a105fc657b8aa245c1ac13b927c7
Madeline Binion <mabinion@iu.edu>
2019-09-16 11:12:43 -0400

    Added Form and Data Issues
    The data is not fetching, but I have everything updated from class, but the only issue is the data not showing up.

---
+++ app/public/dummy.php
@@ -0,0 +1,23 @@
+[
+  {
+    "paitentGuid": "2375941234",
+    "firstName": "Carly",
+    "lastName": "Klutz",
+    "dob": "2000-03-04",
+    "sexAtBirth": "F",
+  },
+  {
+    "paitentGuid": "2749671234",
+    "firstName": "Maggie",
+    "lastName": "Luis",
+    "dob": "1980-10-20",
+    "sexAtBirth": "F"
+  },
+  {
+    "paitentGuid": "2938461234",
+    "firstName": "Mark",
+    "lastName": "Hughes",
+    "dob": "1995-04-13",
+    "sexAtBirth": "M"
+  }
+]
