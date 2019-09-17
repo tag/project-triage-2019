96b5740e9f3a2675a7b5a0257bf9c546bffe17e7
Roxie Liu <roxiliu@iu.edu>
2019-09-17 15:33:31 -0400

    9/16 update

---
+++ app/public/js/record.app.js
@@ -0,0 +1,53 @@
+var patientRecordApp = new Vue({
+  el: '#patientRecordApp',
+  data: {
+    patients:[]
+        },
+  methods:
+  {
+    fetchPatients()
+    {
+      fetch('dummy.php')
+      .then( response => response.json())
+      .then( json => {patientRecordApp.patients = json})
+      ;
+      // .catch(/*function goes here*/)
+      //  thenDoThis();
+
+      // Means the same at this
+      // fetch('https://randomuser.me/api/')
+      // .then(function(response) {return response.json()})
+      // .then(function(json) {waitingApp.people = json});
+    }
+  },
+
+  created()
+  {
+    this.fetchPatients();
+  }
+
+})
+
+// var waitingApp = new Vue({
+//   el: '#patientWaitingApp',
+//   data: {
+//     patients: []
+//   },
+//   methods: {
+//     fetchPatients() {
+//       fetch('dummy.php')
+//       .then(response => response.json())
+//       .then(json => {waitingApp.patients = json});
+//
+//       // Means the same at this
+//       // fetch('https://randomuser.me/api/')
+//       // .then(function(response) {return response.json()})
+//       // .then(function(json) {waitingApp.people = json});
+//
+//     }
+//   },
+//   created: function() {
+//     this.fetchPatients();
+//   }
+//
+// })
