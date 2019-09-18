440dd862086020f7284e64acac6f7ab72acae233
Roxie Liu <roxiliu@iu.edu>
2019-09-17 20:34:43 -0400

    delete something useless

--- app/public/js/record.app.js
+++ app/public/js/record.app.js
@@ -11,13 +11,6 @@ var patientRecordApp = new Vue({
       .then( response => response.json())
       .then( json => {patientRecordApp.patients = json})
       ;
-      // .catch(/*function goes here*/)
-      //  thenDoThis();
-
-      // Means the same at this
-      // fetch('https://randomuser.me/api/')
-      // .then(function(response) {return response.json()})
-      // .then(function(json) {waitingApp.people = json});
     }
   },
 
