d24b3c80f4c150b41536769268e7456218833b25
Tom G <tomg@byu.net>
2019-09-18 10:01:31 -0400

    Event handling, model binding

--- app/public/js/record.app.js
+++ app/public/js/record.app.js
@@ -1,13 +1,34 @@
 var patientRecordApp = new Vue({
   el: '#patientRecordApp',
   data: {
-    patients: []
+    patients: [],
+    formPatient: {
+      firstName: '',
+      lastName: '',
+      dob: '',
+      sexAtBirth: ''
+    }
   },
   methods: {
     fetchPatients() {
       fetch('dummy.php')
       .then(response => response.json())
       .then(json => { patientRecordApp.patients = json })
+    },
+    handleCreateRecord(event) {
+      // TODO: POST the form contents somewhere
+      // fetch(url, {method:"POST", data:this.formPatient})
+      // .then(...)
+      this.patients.push(this.formPatient);
+      this.formPatient = {
+        firstName: '',
+        lastName: '',
+        dob: '',
+        sexAtBirth: ''
+      }
+    },
+    selectRecord(patient) {
+      console.log(patient);
     }
   },
   created() {
