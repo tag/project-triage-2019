8fbec0f6d3733c059f3d086921f4553053e04867
Tom G <tomg@byu.net>
2019-09-18 13:18:24 -0400

    v-model and events

--- app/public/js/record.app.js
+++ app/public/js/record.app.js
@@ -1,13 +1,35 @@
 var patientRecordApp = new Vue({
   el: '#patientRecordApp',
   data: {
-    patients: []
+    patients: [],
+    formPatient: {
+      firstName: '',
+      lastName: '',
+      dob: '',
+      sexAtBirth: ''
+    }
   },
   methods: {
     fetchPatients() {
       fetch('dummy.php')
       .then(response => response.json())
       .then(json => { patientRecordApp.patients = json })
+    },
+    handleCreateRecord(event) {
+      // TODO: Post data to API
+      // fetch(url, {method:'post', data: this.formPatient})
+      // .then(...)
+      this.patients.push(this.formPatient);
+      this.formPatient = {
+        firstName: '',
+        lastName: '',
+        dob: '',
+        sexAtBirth: ''
+      }
+    },
+    handleRecordClick(patient) {
+      // TODO: Pass this data to another Vue app
+      console.log(patient);
     }
   },
   created() {
