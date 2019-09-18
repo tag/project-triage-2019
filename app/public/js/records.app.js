154a9b2ccef1b3a4c1b8c373a5a19fe295bcaff5
Tom G <tomg@byu.net>
2019-09-18 18:25:35 -0400

    v-model & submit event

--- app/public/js/records.app.js
+++ app/public/js/records.app.js
@@ -1,16 +1,35 @@
 var patientRecordsApp = new Vue({
   el: '#patientRecordsApp',
   data: {
-    patients: []
+    patients: [],
+    recordPatient: {}
   },
   methods: {
     fetchPatients() {
       fetch('dummy.php')
       .then(response => response.json())
       .then(json => { patientRecordsApp.patients = json })
+    },
+    handleSubmit(event) {
+      // fetch(url, {
+      //   method: 'post',
+      //   data: this.recordPatient
+      // })
+      // .then( ... )
+      this.patients.push( this.recordPatient );
+      this.handleReset();
+    },
+    handleReset() {
+      this.recordPatient = {
+        firstName: '',
+        lastName: '',
+        dob: '',
+        sexAtBirth: ''
+      }
     }
-  },
+  }, // end methods
   created() {
+    this.handleReset();
     this.fetchPatients();
   }
 });
