db08ba3b3618d8641d09b06676bfc632cb2d058f
Cameron Cline <camcline@iu.edu>
2019-09-18 10:58:55 -0400

    added tom's info to master branch

---
+++ app/public/js/record.app.js
@@ -0,0 +1,22 @@
+var patientRecordApp = new Vue({
+  el: '#patientRecordApp',
+  data: {
+    patients: [],
+    formPatient: {
+      firstName: '',
+      lastName: '',
+      dob: '',
+      sexAtBirth: ''
+    }
+  },
+  methods: {
+    fetchPatients() {
+      fetch('dummy.php')
+      .then(response => response.json())
+      .then(json => { patientRecordApp.patients = json })
+    }
+  },
+  created() {
+    this.fetchPatients();
+  }
+});
