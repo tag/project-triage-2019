e0c71f822f9748afdacc409c4703975bb8b44325
Madeline Binion <mabinion@iu.edu>
2019-09-18 11:23:59 -0400

    View Model and Two Way Binding
    During class I followed along to add view model, binding and fixed my CSS to show colors now.

--- app/public/dummy.php
+++ app/public/dummy.php
@@ -4,20 +4,23 @@
     "firstName": "Carly",
     "lastName": "Klutz",
     "dob": "2000-03-04",
-    "sexAtBirth": "F"
+    "sexAtBirth": "F",
+    "priority":"high"
   },
   {
     "patientGuid": "2729671234",
     "firstName": "Maggie",
     "lastName": "Luis",
     "dob": "1980-10-20",
-    "sexAtBirth": "F"
+    "sexAtBirth": "F",
+    "priority": "medium"
   },
   {
     "patientGuid": "2938461234",
     "firstName": "Mark",
     "lastName": "Hughes",
     "dob": "1995-04-13",
-    "sexAtBirth": "M"
+    "sexAtBirth": "M",
+    "priority": "low"
   }
 ]
