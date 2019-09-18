1d3d9ab9f6c4f5fac153be75274537feec066b84
Roxie Liu <roxiliu@iu.edu>
2019-09-18 12:31:01 -0400

    Update dummyJason.php
    update

--- app/public/dummyJason.php
+++ app/public/dummyJason.php
@@ -4,20 +4,23 @@
     "firstName": "Jason",
     "lastName": "Dummuy",
     "dob": "2006-09-03",
-    "sexAtBirth": "Male"
-  }，
+    "sexAtBirth": "Male",
+    "priority": "high"
+  },
   {
     "patientGuid": "SOME-REALLY-SHORT-2",
     "firstName": "Roxie",
     "lastName": "Liu",
     "dob": "2009-09-03",
-    "sexAtBirth": "Male"
-  }，
+    "sexAtBirth": "Male",
+    "priority": "medium"
+  },
   {
     "patientGuid": "SOME-REALLY-SHORT-3",
     "firstName": "Griffin",
     "lastName": "Jung",
     "dob": "1998-09-03",
-    "sexAtBirth": "Male"
+    "sexAtBirth": "Male",
+    "priority": "low"
   }
 ]
