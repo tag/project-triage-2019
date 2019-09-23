eb889240d652fda0ac74e8ebef978cb9faa45b2a
Terry Bae <junhbae@iu.edu>
2019-09-23 14:26:22 -0400

    changed php and env file

--- app/public/js/waiting.app.js
+++ app/public/js/waiting.app.js
@@ -5,7 +5,7 @@ var waitingApp = new Vue({
   },
   methods: {
     fetchPatients() {
-      fetch('dummy.php')
+      fetch('patients.php')
       .then(response => response.json())
       .then(json => { waitingApp.patients = json })
     }
