51b277db5519996533666e41b6f77a1f636dab56
Tom G <tomg@byu.net>
2019-10-07 13:59:15 -0400

    Records POST, GUID

--- app/public/js/record.app.js
+++ app/public/js/record.app.js
@@ -11,12 +11,20 @@ var patientRecordsApp = new Vue({
       .then(json => { patientRecordsApp.patients = json })
     },
     handleSubmit(event) {
-      // fetch(url, {
-      //   method: 'post',
-      //   data: this.recordPatient
-      // })
-      // .then( ... )
-      this.patients.push( this.recordPatient );
+      fetch('api/records/post.php', {
+        method:'POST',
+        body: JSON.stringify(this.recordPatient),
+        headers: {
+          "Content-Type": "application/json; charset=utf-8"
+        }
+      })
+      .then( response => response.json() )
+      .then( json => { patientRecordsApp.patients.push( json[0] ) })
+      .catch( err => {
+        console.error('RECORD POST ERROR:');
+        console.error(err);
+     });
+
       this.handleReset();
     },
     handleReset() {
