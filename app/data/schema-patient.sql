70ec068749e7fafaedb8cd0c2caca03657bffcf4
bliow <54509960+bliow@users.noreply.github.com>
2019-10-07 11:04:50 -0400

    update to match

--- app/data/schema-patient.sql
+++ app/data/schema-patient.sql
@@ -14,9 +14,14 @@ INSERT INTO Patient (patientGuid, firstName, lastName, dob, sexAtBirth) VALUES
 ("SOME-UNIQUE-ABCDE1", "J", "Doe", "1950-00-00",  ""),
 ("SOME-DUMMY-DATA", "Pepper", "Potts", "1990-01-31",  "F");
 
-CREATE TABLE PatientVisit
-  visitId INTEGER PRIMARY KEY AUTO_INCREMENT,
-  patientGuid VARCHAR(64) UNIQUE,
-  visitDescription TEXT NOT NULL,
-  visitDateUtc DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
-  
+CREATE TABLE PatientVisit (
+    visitId INTEGER PRIMARY KEY AUTO_INCREMENT,
+    patientGuid VARCHAR(64) UNIQUE,
+    visitDescription TEXT NOT NULL,
+    visitDateUtc DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    priority ENUM('low', 'medium', 'high') NOT NULL DEFAULT 'low'
+
+);
+
+INSERT INTO PatientVisit (visitId, patientGuid, visitDescription) VALUES
+(1, 'SOME-REALLY-LONG-1234', 'Anxiety from D&S');
--- app/public/api/waiting/post.php
+++ app/public/api/waiting/post.php
@@ -1,41 +1,18 @@
 <?php
-//everything is going to be php
-
+// Step 0: Validation
 // Step 1: Get a datase connection from our help class
 $db = DbConnection::getConnection();
-//$db = variable
-// -> instance, :: static elements
-
 // Step 2: Create & run the query
 $stmt = $db->prepare(
   'INSERT INTO PatientVisit
     (patientGuid, visitDescription, priority)
-  VALUES (?,?,?)'
+  VALUES (?, ?, ?)'
 );
-// call prepare method - pulls data from patient
-//HOW TO PREVENT SQL INJECTION
 $stmt->execute([
   $_POST['patientGuid'],
   $_POST['visitDescription'],
   $_POST['priority']
 ]);
-// execute the stmt - can add factors?
-// $patients = $stmt->fetchAll();
-//access with fetchAll - get all rows back - each item - associate array with name and value
-
-// patientGuid VARCHAR(64) PRIMARY KEY,
-// firstName VARCHAR(64),
-// lastName VARCHAR(64),
-// dob DATE DEFAULT NULL,
-// sexAtBirth CHAR(1) DEFAULT ''
-
-// Step 3: Convert to JSON
-// $json = json_encode($patients, JSON_PRETTY_PRINT);
-// Pretty print = indent and new lines - easier to read - debuggin purposes
-
 // Step 4: Output
 header('HTTP/1.1 303 See Other');
-header('Location: ../waiting/')
-// header('Content-Type: application/json');
-//  header - setting http header - /type  - eg: image/png, text/css
-// echo $json;
+header('Location: ../waiting/');
