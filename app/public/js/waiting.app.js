f7285a5c2698bb0c9838958ed23df147aca93536
Tom G <tomg@byu.net>
2019-10-14 11:29:24 -0400

    time and filter

--- app/public/js/waiting.app.js
+++ app/public/js/waiting.app.js
@@ -8,6 +8,12 @@ var waitingApp = new Vue({
       fetch('api/waiting/')
       .then(response => response.json())
       .then(json => { waitingApp.patients = json })
+    },
+    displayWaitingLocalDate(d) {
+      return moment.utc(d).local().format("HH:mm MMM Do");
+    },
+    displayWaitingSince(d) {
+      return moment.utc(d).local().fromNow();
     }
   },
   created() {
